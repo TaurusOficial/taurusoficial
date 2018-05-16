00ffecfb31431312c7ec159da01de6e834cca24c
Igor Macedo <igorantonio50@hotmail.com>
2018-05-16 10:03:25 -0300

    Alterações Mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$( document ).ready(function() {
-
+$(document).ready(function () {
+    $('#video').get(0).play();
     scaleVideoContainer();
 
     initBannerVideoSize('.video-container .poster img');
     initBannerVideoSize('.video-container .filter');
     initBannerVideoSize('.video-container video');
 
-    $(window).on('resize', function() {
+    $(window).on('resize', function () {
         scaleVideoContainer();
         scaleBannerVideoSize('.video-container .poster img');
         scaleBannerVideoSize('.video-container .filter');
@@ -19,13 +19,13 @@ function scaleVideoContainer() {
 
     var height = $(window).height() + 5;
     var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
+    $('.homepage-hero-module').css('height', unitHeight);
 
 }
 
-function initBannerVideoSize(element){
+function initBannerVideoSize(element) {
 
-    $(element).each(function(){
+    $(element).each(function () {
         $(this).data('height', $(this).height());
         $(this).data('width', $(this).width());
     });
@@ -34,24 +34,24 @@ function initBannerVideoSize(element){
 
 }
 
-function scaleBannerVideoSize(element){
+function scaleBannerVideoSize(element) {
 
     var windowWidth = $(window).width(),
-    windowHeight = $(window).height() + 5,
-    videoWidth,
-    videoHeight;
+        windowHeight = $(window).height() + 5,
+        videoWidth,
+        videoHeight;
 
     // console.log(windowHeight);
 
-    $(element).each(function(){
-        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+    $(element).each(function () {
+        var videoAspectRatio = $(this).data('height') / $(this).data('width');
 
         $(this).width(windowWidth);
 
-        if(windowWidth < 1000){
+        if (windowWidth < 1000) {
             videoHeight = windowHeight;
             videoWidth = videoHeight / videoAspectRatio;
-            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+            $(this).css({ 'margin-top': 0, 'margin-left': -(videoWidth - windowWidth) / 2 + 'px' });
 
             $(this).width(videoWidth).height(videoHeight);
         }
